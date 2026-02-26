16362de4f21b5f2da386d408b4c68101ce35d596
Victor Morakinyo <morakinyovictor1@gmail.com>
2026-02-26 16:05:47 +0100

    banner image

--- app/Validators/BannerValidator.php
+++ app/Validators/BannerValidator.php
@@ -10,7 +10,7 @@ class BannerValidator
     public function createBannerValidator($request)
     {
         $validationRules = [
-            'banner_image' => 'required|file|mimes:jpg,jpeg,png,gif,heic|max:5120',
+            'banner_image' => 'required|file|max:5120',
             'external_link' => 'required|string|url',
             // 'audience' => 'required|array|min:5',
             'budget' => 'required|string',
