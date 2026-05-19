158f611251b837b8e193e81f54598a05ecd91864
Victor Morakinyo <morakinyovictor1@gmail.com>
2026-05-19 09:24:58 +0100

    email update

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -7,49 +7,90 @@
         <tr>
             <td style="padding: 30px 30px 15px 30px;">
                 <h2 style="font-size: 18px; color: #6576ff; font-weight: 600; margin: 0;">
-                    Enjoy unlimited earnings in Dollars and Naira on Freebyz
+                    Enjoy unlimited Micro Tasks and Remote Job Opportunities on Freebyz Remote Jobs
                 </h2>
             </td>
         </tr>
+
         <tr>
             <td style="padding: 30px 30px 20px">
-                <p style="margin-bottom: 10px;">Hi <strong>{{ $name }},</strong></p>
-                <p style="margin-bottom: 10px;">
-                    We are delighted to welcome you to Freebyz -Africa's premier remote jobs platform connecting you to global earning opportunities! 
-                </p>
-                <p style="margin-bottom: 10px;">
-                    You can earn in both dollars and Naira, regardless of your tech expertise. 
-                </p>
-                <p style="margin-bottom: 10px;">
-                    No tech skills? No worries! Thousands of easy-to-do jobs are updated regularly for fresh opportunities!
-                </p>
-                <p style="margin-bottom: 10px;">
-                    🚀 Freebyz spans across 100+ countries, offering you a chance to be part of the fastest-growing remote job community in Africa. Seamlessly withdraw earnings to PayPal or local bank accounts every Friday using our advanced virtual wallet technology.
-                </p>
-                <p style="margin-bottom: 10px;">
-                    💰 Want to fast-track earnings? Earn up to N11,500 monthly with our referral program! Get a whopping 50% commission on referrals and win fantastic prizes by inviting friends.
-                </p>
-                <p style="margin-bottom: 10px;">
-                    🔥 Withdrawals are processed every Friday. Don't miss out! Copy your referral link and spread the word on WhatsApp, TikTok, Facebook, YouTube, and more! Start your earning journey with Freebyz today!
-                </p>
-               
 
                 <p style="margin-bottom: 10px;">
-                    Don’t linger any further, learn how to complete tasks and earn <a href="https://vm.tiktok.com/ZM6f5mU2a/" target="_blank">Here</a>
+                    Hi <strong>{{ $name }},</strong>
                 </p>
 
                 <p style="margin-bottom: 10px;">
-                    Don’t miss any update from us, follow our Facebook page <a href="https://www.facebook.com/profile.php?id=61554693125457&mibextid=vk8aRt" target="_blank">Here</a>
-                </p>
-              
-                <p style="margin-bottom: 10px;">
-                    <i>See you on your withdrawal day!</i>
+                    You've made an awesome choice to Sign up on Freebyz Remote Jobs.
                 </p>
 
-                
-                {{-- <p style="margin-bottom: 10px;">Its clean, minimal and pre-designed email template that is suitable for multiple purposes email template.</p>
-                <p style="margin-bottom: 15px;">Hope you'll enjoy the experience, we're here if you have any questions, drop us a line at info@yourwebsite.com anytime. </p> --}}
-                <p style="margin-top: 45px; margin-bottom: 15px;">---- <br> Regards, <br><i>Farohunbi Samuel, <br> CEO, Dominahl Technologies.</i></p>
+                <p style="margin-bottom: 10px;">
+                    I am delighted to welcome you to Africa's Micro & Remote Jobs platform connecting you to <b>Micro Tasks, Gigs, Job Vacancies and Global earning opportunities!</b>
+                </p>
+
+                <p style="margin-bottom: 10px;">
+                    At Freebyz Remote Jobs, we provide simple tasks that can be completed in a few minutes and Gigs to earn in your local currency as well as No-experience Jobs, Experienced Jobs, Remote Jobs, Part Time Jobs, Internships and other Opportunities accessible on your mobile phone (on the  <a href="https://dashboard.freebyz.com/login" target="_blank">web</a> and on our  <a href="https://play.google.com/store/apps/details?id=com.mobile.freebyz" target="_blank">Mobile App</a>).
+                </p>
+
+                <p style="margin-bottom: 10px;">
+                    You don't have tech skills to get highly paid? No worries!
+                </p>
+
+                <p style="margin-bottom: 10px;">
+                    We've got hundreds of easy-to-do tasks updated regularly on Freebyz Remote Jobs!
+                </p>
+
+                <p style="margin-bottom: 10px;">
+                    🚀 Our 400K+ user base spans across 100+ countries, offering you a chance to be part of the fastest-growing remote job community in Africa. You can seamlessly withdraw earnings from micro tasks to PayPal or your local bank account using our advanced virtual wallet technology.
+                </p>
+
+                <p style="margin-bottom: 10px;">
+                    💰 We list only verified and highly paid Remote and Full Time Job Opportunities.
+                </p>
+
+                <p style="margin-bottom: 10px;">
+                    🔥 Withdrawals of earnings on Micro Tasks are processed every Friday.
+                </p>
+
+                <p style="margin-bottom: 20px;">
+                    Don't miss out!
+                </p>
+
+                <p style="margin-bottom: 10px;">
+                    You can invite your friends and earn fantastic rewards for this!
+                </p>
+
+                <p style="margin-bottom: 10px;">
+                     <a href="https://dashboard.freebyz.com/login" target="_blank">Login now</a> or  <a href="https://play.google.com/store/apps/details?id=com.mobile.freebyz" target="_blank">Download our App</a> to copy your referral link and spread the word on WhatsApp, TikTok, Facebook, YouTube, and more!
+                </p>
+
+                <p style="margin-bottom: 10px;">
+                    Don’t linger any further, Learn how to complete tasks and earn
+                    <a href="https://vm.tiktok.com/ZM6f5mU2a/" target="_blank">Here</a>
+                </p>
+
+                <p style="margin-bottom: 10px;">
+                    Don’t miss any update from us, Follow our WhatsApp Channel
+                    <a href="https://whatsapp.com/channel/0029Vb7Zfnb65yDGlRg8ho1M" target="_blank">Here</a>
+                </p>
+
+                <p style="margin-bottom: 10px;">
+                    Don't miss your chance to land your dream Job!
+                </p>
+
+                <p style="margin-bottom: 10px;">
+                    <i>You are amazing!</i>
+                </p>
+
+                <p style="margin-top: 45px; margin-bottom: 15px;">
+                    {{-- ---- <br>
+                    Regards, <br> --}}
+                    ----
+                    <i>
+                        Dr. Samuel FAROHUNBI, <br>
+                        CEO, Freebyz Technologies Ltd.
+                    </i>
+                </p>
+
             </td>
         </tr>
     </tbody>
