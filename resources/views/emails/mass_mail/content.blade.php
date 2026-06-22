269a94b439e42bf1fb3c67c0b63aad986eafaa4d
Victor Morakinyo <morakinyovictor1@gmail.com>
2026-06-22 14:20:33 +0100

    emails

--- resources/views/emails/mass_mail/content.blade.php
+++ resources/views/emails/mass_mail/content.blade.php
@@ -13,14 +13,14 @@
             <td style="padding: 30px 30px 20px">
                 <p style="margin-bottom: 10px;">Hi <strong>{{ $name }},</strong></p>
                 <p style="margin-bottom: 10px;">
-                {!! $message !!}    
+                {!! $message !!}
                 <br>
-                    
+
                 </p>
                 <p style="margin-bottom: 10px;">
-                    Click the button below to access more jobs... <br><br>
-                    <a href="{{ url('home') }}" target="_blank" style="background-color:#6576ff;border-radius:4px;color:#ffffff;display:inline-block;font-size:13px;font-weight:600;line-height:44px;text-align:center;text-decoration:none;text-transform: uppercase; padding: 0 30px">
-                        Take More Jobs
+                    Click the button below to access more Tasks... <br><br>
+                    <a href="https://dashboard.freebyz.com/tasks" target="_blank" style="background-color:#6576ff;border-radius:4px;color:#ffffff;display:inline-block;font-size:13px;font-weight:600;line-height:44px;text-align:center;text-decoration:none;text-transform: uppercase; padding: 0 30px">
+                        Take More Tasks
                     </a>
                 </p>
                 {{-- <p style="margin-bottom: 10px;">Its clean, minimal and pre-designed email template that is suitable for multiple purposes email template.</p>
